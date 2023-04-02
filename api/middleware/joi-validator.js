6f4ef2936ec53d101664b85bd7ea3bcc29106c35
Gaurav Dubey <gaurav.dubey@leverageedu.com>
2023-04-02 15:02:20 +0530

    validation err response changes:

--- api/middleware/joi-validator.js
+++ api/middleware/joi-validator.js
@@ -2,7 +2,9 @@ module.exports = (schema) => (req, res, next) => {
   try {
     const { error } = schema.validate(req.body);
     if (error) {
-      return res.status(422).send(error.details[0].message);
+      return res.status(422).json({
+        validation: error.details,
+      });
     } else {
       next();
     }
